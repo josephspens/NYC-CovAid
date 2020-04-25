39a54106913e36f51e31b01f36ed8abf4bdcdb44
Joseph Spens <jpspens@gmail.com>
2020-04-25 18:08:28 -0400

    Add filters

---
+++ src/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Filters from '../Filters';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Filters />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Filters/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Navbar } from 'react-bootstrap';
+
+export default function () {
+  return (
+    <Container style={{ paddingTop: 20 }}>
+        <Navbar bg="light">
+            Filters
+        </Navbar>
+    </Container>
+  );
+}
