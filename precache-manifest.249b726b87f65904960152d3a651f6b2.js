017def348f4d470699220c0644be9331734978be
Joseph Spens <jpspens@gmail.com>
2020-04-26 12:40:07 -0400

    Updates

---
+++ precache-manifest.249b726b87f65904960152d3a651f6b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7411169376d21204714ed7b019e2c528",
+    "url": "/NYC-CovAid/index.html"
+  },
+  {
+    "revision": "fdeb3d2f37deacecb62c",
+    "url": "/NYC-CovAid/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a5736ad0d96c1a5b3b74",
+    "url": "/NYC-CovAid/static/css/main.119699df.chunk.css"
+  },
+  {
+    "revision": "fdeb3d2f37deacecb62c",
+    "url": "/NYC-CovAid/static/js/2.f05917e1.chunk.js"
+  },
+  {
+    "revision": "1adb110813a30163ca679a14e0de12c3",
+    "url": "/NYC-CovAid/static/js/2.f05917e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5736ad0d96c1a5b3b74",
+    "url": "/NYC-CovAid/static/js/main.77947851.chunk.js"
+  },
+  {
+    "revision": "39f39b4d085a013852e4",
+    "url": "/NYC-CovAid/static/js/runtime-main.0ef06f74.js"
+  }
+]);
